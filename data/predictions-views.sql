7b91e20abf15e3365be71a8b262178b0aba89bed
index-git <sileny.index@gmail.com>
2022-04-22 16:50:12 +0200

    Calculate predictions for all rows and export to DB

--- data/predictions-views.sql
+++ data/predictions-views.sql
@@ -5,3 +5,11 @@ select ta.fid, ta.trenovacitypkod, te.chronotyp_guessed, ta.geom
 from train_rows_all_columns ta
 left join train_rows_predictions te on (ta.fid = te.fid)
 ;
+
+DROP MATERIALIZED VIEW IF EXISTS all_rows_predictions_geom CASCADE;
+create MATERIALIZED view all_rows_predictions_geom
+AS
+select ta.fid, ta.trenovacitypkod, te.chronotyp_guessed, ta.geom
+from all_rows_all_columns ta
+left join all_rows_predictions te on (ta.fid = te.fid)
+;
