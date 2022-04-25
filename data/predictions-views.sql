20dc16c991e6afca269a314f5b473b767a09f037
index-git <sileny.index@gmail.com>
2022-04-25 16:52:46 +0200

    Add full (joint) geometry for buildings

--- data/predictions-views.sql
+++ data/predictions-views.sql
@@ -1,7 +1,8 @@
 DROP MATERIALIZED VIEW IF EXISTS joint_rows_predictions_geom CASCADE;
 create MATERIALIZED view joint_rows_predictions_geom
 AS
-select ta.kod, ta.trenovacitypkod, te.chronotyp_guessed, ta.geom
+select ta.kod, ta.trenovacitypkod, te.chronotyp_guessed, ru.geom
 from joint_rows_all_columns ta
+inner join joint_rows_ruian ru on ru.kod = ta.kod
 left join joint_rows_predictions te on (ta.kod = te.kod)
 ;
