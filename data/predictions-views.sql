f9adc4ab34afbf53d05767b295c57635c9d9326e
index-git <sileny.index@gmail.com>
2022-04-25 10:08:18 +0200

    Join same buildings into one row

--- data/predictions-views.sql
+++ data/predictions-views.sql
@@ -1,15 +1,7 @@
-DROP MATERIALIZED VIEW IF EXISTS train_rows_predictions_geom CASCADE;
-create MATERIALIZED view train_rows_predictions_geom
+DROP MATERIALIZED VIEW IF EXISTS joint_rows_predictions_geom CASCADE;
+create MATERIALIZED view joint_rows_predictions_geom
 AS
-select ta.fid, ta.trenovacitypkod, te.chronotyp_guessed, ta.geom
-from train_rows_all_columns ta
-left join train_rows_predictions te on (ta.fid = te.fid)
-;
-
-DROP MATERIALIZED VIEW IF EXISTS all_rows_predictions_geom CASCADE;
-create MATERIALIZED view all_rows_predictions_geom
-AS
-select ta.fid, ta.trenovacitypkod, te.chronotyp_guessed, ta.geom
-from all_rows_all_columns ta
-left join all_rows_predictions te on (ta.fid = te.fid)
+select ta.kod, ta.trenovacitypkod, te.chronotyp_guessed, ta.geom
+from joint_rows_all_columns ta
+left join joint_rows_predictions te on (ta.kod = te.kod)
 ;
