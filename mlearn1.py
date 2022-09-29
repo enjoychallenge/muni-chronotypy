a315176973211d2ef5fc98eae5463a25b91bdcfc
index-git <sileny.index@gmail.com>
2022-09-29 14:54:18 +0200

    Print top 20 attribute correlations

--- mlearn1.py
+++ mlearn1.py
@@ -68,6 +68,17 @@ logger.info('*******************************************************************
 correlations = dataset.corr(method='pearson')
 logger.info(f'Show correlation between attributes\n{correlations}')
 
+unstack_correlations = correlations.abs().unstack()
+logger.info(f'unstack_correlations\n{unstack_correlations}')
+pairs_to_drop = set()
+cols = correlations.columns
+for i in range(0, correlations.shape[1]):
+    for j in range(0, i + 1):
+        pairs_to_drop.add((cols[i], cols[j]))
+au_corr = unstack_correlations.drop(labels=pairs_to_drop).sort_values(ascending=False)
+# au_corr = unstack_correlations.sort_values(ascending=False)
+logger.info(f'Highest correlations\n{au_corr[0:20]}')
+
 logger.info('****************************************************************************************************')
 logger.info(f'Show attribute skewness\n{dataset.skew()}')
 
